b6985e93bdbd20dc89bc2a0b1d3e302591df1d53
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-10-17 14:15:56 -0500

    refactor: remove unnecesary else

--- main.ts
+++ main.ts
@@ -1,43 +1,58 @@
 let sock: WebSocket | null = null;
+
 const reqMap: Map<string, (res: Response) => void> = new Map();
+
 export default {
   async fetch(req: Request) {
     if (req.headers.get("upgrade") === "websocket") {
       const { socket, response } = Deno.upgradeWebSocket(req);
+
       socket.addEventListener("open", () => {
         if (sock) {
           console.log("Closing existing WebSocket connection");
           socket.close();
           return;
         }
+
         sock = socket;
       });
+
       socket.addEventListener("message", (event) => {
         const data = JSON.parse(event.data);
         const resolve = reqMap.get(data.id);
+
         if (resolve) {
           const response = new Response(data.body, {
             status: data.status,
             headers: Object.fromEntries(data.headers),
           });
+
           resolve(response);
           reqMap.delete(data.id);
         }
       });
+
       socket.addEventListener("close", () => {
         if (sock === socket) sock = null;
       });
+
       return response;
     }
+
     const url = new URL(req.url);
+
     if (url.pathname === "/client.ts") {
       return new Response(await Deno.readTextFile("./client.ts"));
     }
+
     if (sock) {
       console.log("Forwarding request via WebSocket:", req.url);
+
       const { promise, resolve } = Promise.withResolvers<Response>();
       const reqId = crypto.randomUUID();
+
       reqMap.set(reqId, resolve);
+
       sock.send(
         JSON.stringify({
           id: reqId,
@@ -48,9 +63,8 @@ export default {
         }),
       );
       return promise;
-    } else {
-      console.log("WebSocket not connected");
-      return new Response("Not Found", { status: 404 });
     }
+    console.log("WebSocket not connected");
+    return new Response("Not Found", { status: 404 });
   },
 };
